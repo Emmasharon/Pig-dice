fd956fb4186004c929946595d314b172933c0f75
Emma Sharon <emmasharon19@gmail.com>
2019-05-12 23:47:22 +0300

    link html to js file

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(#submit).submit(function(event){
+  event,preventDefault();
+  num++;
+  if (num>2){
+    alert("only two players please");
+    reset();
+  }
+  else if (num==2) {
+    $
+  }
+}
