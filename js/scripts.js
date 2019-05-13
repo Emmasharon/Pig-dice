34335c0c386d5ec0854700087cc8b2944cb6938e
Emma Sharon <emmasharon19@gmail.com>
2019-05-13 12:33:57 +0300

    add function for player 2

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $("#play2").click(function(){
   $(".para2").fadeIn();
 });
 
- var name1 , cummulativeRoll1, total1,  name2 , cummulativeRoll2, total2,  ;
+ var name1 , cummulativeRoll1, total1,  name2 , cummulativeRoll2, total2  ;
 
  total1 = 0;
  cummulativeRoll1 = 0;
@@ -16,149 +16,51 @@ $("#play2").click(function(){
  cummulativeRoll2 = 0;
 
 
- $("#roll1").click(function() {
+ $(".roll1").click(function() {
    randomNo = Math.floor(Math.random() * 6) + 1;
 
    if (randomNo === 1) {
      alert("Oops, you rolled a 1");
      cummulativeRoll1 = 0;
      $("#count1").text(cummulativeRoll1);
-     total1 += roundCounts1;
+     total1 += cummulativeRoll1;
      $("#total1").text(total1);
      $(".hide1").hide();
    } else {
-     $("#turn1").text(randomNo);
+     $("#Roll1").text(randomNo);
      cummulativeRoll1 += randomNo;
      $("#count1").text(cummulativeRoll1);
    }
 
  });
-
  $("#hold1").click(function(){
     total1 += cummulativeRoll1;
-    $("#total1").text(totalPoints1);
+    $("#total2").text(totalPoints2);
     cummulativeRoll1 = 0;
     $(".hide1").hide();
  });
+//for player 2//
+ $(".roll2").click(function() {
+   randomNo = Math.floor(Math.random() * 6) + 1;
+
+   if (randomNo === 1) {
+     alert("Oops, you rolled a 1");
+     cummulativeRoll2 = 0;
+     $("#count2").text(cummulativeRoll2);
+     total2 += cummulativeRoll2;
+     $("#total2").text(total2);
+     $(".hide2").hide();
+   } else {
+     $("#Roll2").text(randomNo);
+     cummulativeRoll2 += randomNo;
+     $("#count2").text(cummulativeRoll2);
+   }
+   $("#hold2").click(function(){
+      total2 += cummulativeRoll2;
+      $("#total2").text(totalPoints2);
+      cummulativeRoll2 = 0;
+      $(".hide2").hide();
+   });
 
 });
 });
-
-
-
-
-
-
-
-
-
-//to promp players to insert names//
-
-
-
-
-
-
-
-
-
-
-
-// $("#play").click(function(){
-//   $(".hide1").show();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   if ($("#p1") == "" || $("#p2").val() == "") {
-//   alert("Please fill in your names to continue!");
-//   location.reload();
-//   }
-//   else{
-//    nameInput1 =$("input#p1").val();
-//    nameInput2 =$("input#p2").val();
-//   $("#first").text(nameInput1);
-//   $("#second").text(nameInput2);
-// }
-// })
-//
-//   var totalSum1=0;
-//   var sum1=[];
-//   var currentScore1=0;
-//   var totalSum2=0;
-//   var sum2=[];
-//   var currentScore2=0;
-//
-//
-// $(".roll1")function() {
-//     rollDice1=Math.floor(Math.random() *6) +1;
-//     document.getElementById('#pl1')
-//     if(rollDice1 === 1){
-//       alert("good job!want to roll again?");
-//     }
-
-    // if(rollDice1==6){
-    //   alert("good job!want to roll again?");
-    // }
-    // else if(rollDice1==2){
-    //   alert("good job!want to roll again?");
-    // }
-    // else if(rollDice1==3){
-    //   alert("good job!want to roll again?");
-    // }
-    // else if(rSSollDice1==4){
-    //   alert("good job!want to roll again?");
-    // }
-    // else if(rollDice1==5){
-    //   alert("good job!want to roll again?");
-    // }
-    // else if (rollDice1==1){
-    //   alert("Oops! That's a 1")
-    // }
-    // if(rollDice1!=1){
-    //     currentScore1+=rollDice1;
-    //
-    // }
-    // else{
-    //  currentScore1=0;
-    //  sum1=[];
-    //  $(".bt1").hide();
-    //  $(".bt2").show();
-    //  $("#blue").css({opacity: "0.2"});
-    //  $("#green").css({opacity: "1"});
-    // }
-
-// });
