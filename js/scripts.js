169af276a1c2e2c10fde352d612cfc0f7c72e4bb
Emma Sharon <emmasharon19@gmail.com>
2019-05-13 11:50:46 +0300

    add instructions file

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,46 @@ $(document).ready(function(){
 $("#play1").click(function(){
   $(".hide1").fadeIn();
   $(".para1").fadeIn();
-
 });
 $("#play2").click(function(){
   $(".hide2").fadeIn();
   $(".para2").fadeIn();
 });
 
+ var name1 , cummulativeRoll1, total1,  name2 , cummulativeRoll2, total2,  ;
+
+ total1 = 0;
+ cummulativeRoll1 = 0;
+ total2 = 0;
+ cummulativeRoll2 = 0;
+
+
+ $("#roll1").click(function() {
+   randomNo = Math.floor(Math.random() * 6) + 1;
+
+   if (randomNo === 1) {
+     alert("Oops, you rolled a 1");
+     cummulativeRoll1 = 0;
+     $("#count1").text(cummulativeRoll1);
+     total1 += roundCounts1;
+     $("#total1").text(total1);
+     $(".hide1").hide();
+   } else {
+     $("#turn1").text(randomNo);
+     cummulativeRoll1 += randomNo;
+     $("#count1").text(cummulativeRoll1);
+   }
+
+ });
+
+ $("#hold1").click(function(){
+    total1 += cummulativeRoll1;
+    $("#total1").text(totalPoints1);
+    cummulativeRoll1 = 0;
+    $(".hide1").hide();
+ });
+
+});
 });
 
 
