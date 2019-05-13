ced0cc641d1b3a55f02fc69be045d9af7f965121
Emma Sharon <emmasharon19@gmail.com>
2019-05-13 05:59:07 +0300

    add alert to promp users to fill in mames first

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-$(#submit).submit(function(event){
-  event,preventDefault();
-  num++;
-  if (num>2){
-    alert("only two players please");
-    reset();
+// $(#submit).submit(function(event){
+//   event,preventDefault();
+//   num++;
+//   if (num>2){
+//     alert("only two players please");
+//     reset();
+//   }
+//   else if (num==2) {
+//     $
+//   }
+// }
+
+$("#play").click(function(){
+  if ($("#p1") == "" || $("#p2").val() == "") {
+  alert("Please fill in your names to continue!");
+  location.reload();
   }
-  else if (num==2) {
-    $
-  }
+  else{
+   nameInput1 =$("input#name1").val();
+   nameInput2 =$("input#name2").val();
+  $("#first").text(nameInput1);
+  $("#second").text(nameInput2);
 }
+})
