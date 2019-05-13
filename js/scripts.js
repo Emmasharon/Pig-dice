83f89cacbeeebe5726a7410e3ab6312641475baa
Emma Sharon <emmasharon19@gmail.com>
2019-05-13 11:29:40 +0300

    add display section for game roll

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 $("#play1").click(function(){
   $(".hide1").fadeIn();
+  $(".para1").fadeIn();
+
 });
 $("#play2").click(function(){
   $(".hide2").fadeIn();
+  $(".para2").fadeIn();
 });
 
 });
